738348f292fa0df2d6e72e6e8b91cd40adff1c03
Felix Rieseberg <felix@felixrieseberg.com>
2016-12-05 20:24:47 +0700

    :rocket: First version, dumb as bricks

---
+++ README.md
@@ -0,0 +1,6 @@
+## Atom-Ghost
+
+Write your blog posts in Atom - and push them directly to Ghost Desktop. Requires 1.3 of Ghost Desktop.
+
+## License
+MIT, see LICENSE.md for details.
---
+++ spec/atom-ghost-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomGhostView from '../lib/atom-ghost-view';
+
+describe('AtomGhostView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
